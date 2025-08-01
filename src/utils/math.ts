6361906e4d45d412a6ad94f985ad7737c0eb9f69
randomplayables <randomplayables@proton.me>
2025-08-01 20:38:03 +0000

    feat: Add src/utils/math.ts

---
+++ src/utils/math.ts
@@ -0,0 +1,15 @@
+export function generateRandomNumber(min: number, max: number): number {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+export function isPrime(num: number): boolean {
+  if (num <= 1) return false;
+  if (num <= 3) return true;
+  if (num % 2 === 0 || num % 3 === 0) return false;
+  for (let i = 5; i * i <= num; i += 6) {
+    if (num % i === 0 || num % (i + 2) === 0) {
+      return false;
+    }
+  }
+  return true;
+}
