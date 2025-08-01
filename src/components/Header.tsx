8ed20ef67fa9b185947919247de797ebfc9de944
randomplayables <randomplayables@proton.me>
2025-08-01 20:37:59 +0000

    feat: Add src/components/Header.tsx

---
+++ src/components/Header.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header: React.FC = () => {
+  return (
+    <header className="header">
+      <h1>Prime Guessing Game</h1>
+    </header>
+  )
+}
+
+export default Header
